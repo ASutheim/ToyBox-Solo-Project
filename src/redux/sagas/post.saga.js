0314dbd1f1408322d262c5bafbfca2684bc4bed0
Anna Sutheim <asutheim@gmail.com>
2023-06-27 10:02:06 -0500

    set up post saga

---
+++ src/redux/sagas/post.saga.js
@@ -0,0 +1,18 @@
+import { put, takeLatest } from "redux-saga/effects";
+import axios from "axios";
+
+function* postToy(action) {
+  console.log(action.payload);
+  try {
+    yield axios.post("/api/toys", action.payload);
+    yield put({ type: "GET_USER_TOYS" });
+  } catch {
+    console.log("error with post request in post saga");
+  }
+}
+
+function* postRoot() {
+  yield takeLatest("POST_TOY", postToy);
+}
+
+export default postRoot;
