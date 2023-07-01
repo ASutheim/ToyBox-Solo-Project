e34c6e2115aaafb6d7cacf3a190cbc2668b35394
Anna Sutheim <asutheim@gmail.com>
2023-07-01 11:15:07 -0500

    Set up ToyView detail component, sourced into Toy List and App.jsx. Click on an item within the toy list now routes to a new page.

--- src/components/ToyList/ToyList.jsx
+++ src/components/ToyList/ToyList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector} from "react-redux";
 import { useHistory } from "react-router-dom";
 import "./ToyList.css"
+import ToyView from '../ToyView/ToyView.jsx'
 
 
 function ToyList() {
---
+++ src/components/ToyView/ToyView.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, useHistory } from "react-router-dom";
+
+function ToyView() {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const allToys = useSelector((store) => store.toys);
+  const user = useSelector((store) => store.user);
+  const history = useHistory();
+
+  useEffect(() => {
+    dispatch({ type: "GET_TOY" });
+  }, []);
+
+  console.log("Toy info:");
+
+  return (
+    <div>
+      <p>Inside toy detail view!</p>
+    </div>
+  );
+}
+
+export default ToyView;
