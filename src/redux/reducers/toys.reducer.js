eb82903845b7d9e5da4fae83fb78550cadedcdc2
Anna Sutheim <asutheim@gmail.com>
2023-07-01 12:13:47 -0500

    Sagas and reducers updated to include GET_TOY and SET_TOY for toy detail view

--- src/redux/reducers/toys.reducer.js
+++ src/redux/reducers/toys.reducer.js
@@ -2,6 +2,8 @@ const toyReducer = (state = [], action) => {
   switch (action.type) {
     case "SET_TOYS":
       return action.payload;
+    case "SET_TOY":
+      return action.payload;
     default:
       return state;
   }
--- src/redux/sagas/get.saga.js
+++ src/redux/sagas/get.saga.js
@@ -1,20 +1,29 @@
-import axios from 'axios';
-import { put, takeLatest } from 'redux-saga/effects';
-
+import axios from "axios";
+import { put, takeLatest } from "redux-saga/effects";
 
 function* getToys() {
-console.log("Inside getToys saga")
-    try {
-      const toys = yield axios.get("/api/toys");
-      console.log("Get toys saga returns toy data:", toys.data);
-      yield put({ type: "SET_TOYS", payload: toys.data });
-    } catch {
-      console.log("Error in getToys saga", error);
-    }
+  console.log("Inside getToys saga");
+  try {
+    const toys = yield axios.get("/api/toys");
+    console.log("Get toys saga returns toy data:", toys.data);
+    yield put({ type: "SET_TOYS", payload: toys.data });
+  } catch {
+    console.log("Error in getToys saga", error);
   }
+}
+function* getToy(id) {
+  console.log("Inside saga for getting toy detail view", id.payload);
+  try {
+    const toy = yield axios.get(`/api/toys/${id.payload}`);
+    console.log("Get toys saga returns toy data:", toy.data);
+    yield put({ type: "SET_TOY", payload: toy.data });
+  } catch {
+    console.log("Error in getToy saga", error);
+  }
+}
+function* displaySaga() {
+  yield takeLatest("GET_TOYS", getToys);
+  yield takeLatest("GET_TOY", getToy);
+}
 
-  function* displaySaga() {
-    yield takeLatest('GET_TOYS', getToys);
-  }
-  
-  export default displaySaga;
+export default displaySaga;
