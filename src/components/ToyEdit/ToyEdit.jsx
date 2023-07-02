254bd288bd3622cb3690c8d0e71ca8fc8c137c1f
Anna Sutheim <asutheim@gmail.com>
2023-07-02 16:49:34 -0500

    Click of the delete button in the toy view now opens a confirmation modal

--- src/components/ToyEdit/ToyEdit.jsx
+++ src/components/ToyEdit/ToyEdit.jsx
@@ -13,6 +13,25 @@ function ToyEdit() {
     categories: [],
   });
 
+  const handleAgeOptions = () => {
+    let options = document.getElementsByName("age");
+    let array = toyInfo.age;
+    for (let option of options) {
+      if (option.checked) {
+        array.push(option.value);
+      }
+    }
+  };
+
+  const handleCategoriesOptions = () => {
+    let options = document.getElementsByName("categories");
+    let array = toyInfo.categories;
+    for (let option of options) {
+      if (option.checked) {
+        array.push(option.value);
+      }
+    }
+  };
   const handleEditSubmit = () => {
     console.log("Submit edit button clicked!");
   };
@@ -52,9 +71,101 @@ function ToyEdit() {
         }
       />
 
-        
+      <fieldset id="age" value={toy.age}>
+        <legend>Age:</legend>
+        <label for="age1">
+          <input type="checkbox" id="age" name="age" value="1" />
+          0-2 year olds
+        </label>
+        <label for="age2">
+          <input type="checkbox" id="age" name="age" value="2" /> 2-4 year olds
+        </label>
+        <label for="age3">
+          <input type="checkbox" id="age" name="age" value="3" /> 4-6 year olds
+        </label>
+        <label for="age4">
+          <input type="checkbox" id="age" name="age" value="4" /> 7 and up
+        </label>
+        <label for="age5">
+          <input type="checkbox" id="age" name="age" value="5" /> 10 an up
+        </label>
+        <label for="age6">
+          <input type="checkbox" id="age" name="age" value="6" /> 12 and up
+        </label>
+        <label for="age7">
+          <input type="checkbox" id="age" name="age" value="7" /> Any age!
+        </label>
+      </fieldset>
 
+      <fieldset id="categories" value={toy.categories}>
+        <legend>Categories:</legend>
+        <label for="Outdoors">
+          <input type="checkbox" id="category" name="categories" value="1" />
+          Outdoors
+        </label>
+        <label for="Sports">
+          <input type="checkbox" id="category" name="categories" value="2" />
+          Sports
+        </label>
+        <label for="STEM">
+          <input type="checkbox" id="category" name="categories" value="3" />
+          STEM
+        </label>
+        <label for="Art and Music">
+          <input type="checkbox" id="category" name="categories" value="4" />
+          Art and Music
+        </label>
+        <label for="Language/Reading">
+          <input type="checkbox" id="category" name="categories" value="5" />
+          Language/Reading
+        </label>
+        <label for="Play Pretend">
+          <input type="checkbox" id="category" name="categories" value="6" />
+          Play Pretend
+        </label>
+        <label for="Dolls/Figurines">
+          <input type="checkbox" id="category" name="categories" value="7" />
+          Dolls/Figurines
+        </label>
+        <label for="Animals">
+          <input type="checkbox" id="category" name="categories" value="8" />
+          Animals
+        </label>
+        <label for="Vehicles">
+          <input type="checkbox" id="category" name="categories" value="9" />
+          Vehicles
+        </label>
+        <label for="Tools">
+          <input type="checkbox" id="category" name="categories" value="10" />
+          Tools
+        </label>
+        <label for="Puzzles">
+          <input type="checkbox" id="category" name="categories" value="11" />
+          Puzzles
+        </label>
+        <label for="Games">
+          <input type="checkbox" id="category" name="categories" value="12" />
+          Games
+        </label>
+        <label for="Electronics">
+          <input type="checkbox" id="category" name="categories" value="13" />
+          Electronics
+        </label>
+        <label for="Building">
+          <input type="checkbox" id="category" name="categories" value="14" />
+          Building
+        </label>
+        <label for="Collectibles">
+          <input type="checkbox" id="category" name="categories" value="15" />
+          Collectibles
+        </label>
+        <label for="Sensory">
+          <input type="checkbox" id="category" name="categories" value="16" />
+          Sensory
+        </label>
+      </fieldset>
 
+      <button type="submit">Submit</button>
     </form>
   );
 }
--- src/components/ToyView/ToyView.jsx
+++ src/components/ToyView/ToyView.jsx
@@ -10,22 +10,47 @@ function ToyView() {
   const toy = useSelector((store) => store.toy[0]);
   const user = useSelector((store) => store.user);
 
-  console.log("HEY LOOK HERE:", toy);
-
   useEffect(() => {
     dispatch({ type: "GET_TOY", payload: id });
   }, {});
 
+  const [showModal, setShowModal] = useState(false);
+
+  const handleCancel = () => {
+    setShowModal(false);
+  };
+
+  const handleClickDelete = () => {
+    setShowModal(true);
+  };
+
   const handleDelete = () => {
+    setShowModal(false);
     console.log("Delete button pushed! ID to delete:", id);
     dispatch({ type: "DELETE_TOY", payload: id });
     history.push("/user");
   };
 
+  const DeleteConfirmationModal = ({ onDelete, onCancel }) => {
+    return (
+      <div className="modal">
+        <div className="modal-content">
+          <h3>Confirm Deletion</h3>
+          <p>Are you sure you want to delete this toy?</p>
+          <div className="modal-buttons">
+            <button className="btn-delete" onClick={onDelete}>
+              Delete
+            </button>
+            <button className="btn-cancel" onClick={onCancel}>
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   const [showEdit, setShowEdit] = useState(false);
-
-
-
 
   return (
     <div>
@@ -37,7 +62,7 @@ function ToyView() {
 
       {showEdit ? (
         <div id="edit_view">
-          <ToyEdit/>
+          <ToyEdit />
         </div>
       ) : (
         <div id="info_view">
@@ -53,9 +78,15 @@ function ToyView() {
           <div id="image">
             <img src={toy?.picture_url} />
           </div>
-          <button id="delete" onClick={() => handleDelete(toy.id)}>
+          <button id="delete" onClick={handleClickDelete}>
             Delete this toy
           </button>
+          {showModal && (
+            <DeleteConfirmationModal
+              onDelete={() => handleDelete(toy.id)}
+              onCancel={handleCancel}
+            />
+          )}
         </div>
       )}
     </div>
