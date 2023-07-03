e0b3b731a4ab102de5d207eb0e7728c9e088fbda
Anna Sutheim <asutheim@gmail.com>
2023-07-03 15:52:02 -0500

    Community Toy List added to nav bar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import './Nav.css';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { Link } from "react-router-dom";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import "./Nav.css";
+import { useSelector } from "react-redux";
 
 function Nav() {
   const user = useSelector((store) => store.user);
@@ -25,9 +25,12 @@ function Nav() {
         {user.id && (
           <>
             <Link className="navLink" to="/user">
-              Home
+              My Toy List
             </Link>
 
+            <Link className="navLink" to="/community_toy_list">
+              Community Toy List
+            </Link>
             <Link className="navLink" to="/info">
               Info Page
             </Link>
