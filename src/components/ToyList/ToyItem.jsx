05d6fe6f9ee4d37742d3c09eb4ed8f5d7d7b216e
Anna Sutheim <asutheim@gmail.com>
2023-07-09 10:41:36 -0500

    Toy Item Styling

--- src/components/ToyList/ToyItem.jsx
+++ src/components/ToyList/ToyItem.jsx
@@ -11,25 +11,31 @@ import "./ToyItem.css";
 function ToyItem({ toy, handleNavigateDetailView }) {
   return (
     <div className="card">
-    <Card
-      sx={{ width: 200, height: 300, padding: 1, elevation: 3 }}
-      className="toy-card"
-    >
-      <CardMedia
-        component="img"
-        height="194"
-        image={toy.picture_url}
-        // Make the Image shrink or stretch to fit into the card
-        sx={{ objectFit: "contain" }}
-        // When we want to navigate to the detail view, make sure to pass the toy's id so we can navigate correctly!
-        onClick={() => handleNavigateDetailView(toy.id)}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {toy.name}
-        </Typography>
-      </CardContent>
-    </Card>
+      <Card
+        sx={{
+          width: 200,
+          height: 300,
+          padding: 1,
+          paddingBottom: 0,
+          elevation: 3,
+        }}
+        className="toy-card"
+      >
+        <CardMedia
+          component="img"
+          height="194"
+          image={toy.picture_url}
+          // Make the Image shrink or stretch to fit into the card
+          sx={{ objectFit: "contain" }}
+          // When we want to navigate to the detail view, make sure to pass the toy's id so we can navigate correctly!
+          onClick={() => handleNavigateDetailView(toy.id)}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h6" component="div">
+            {toy.name}
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   );
 }
