97e1435b86933b304fdbb072f803ae14007bc804
Anna Sutheim <asutheim@gmail.com>
2023-07-09 10:41:09 -0500

    Toy Form Styling

--- src/components/ToyForm/ToyForm.jsx
+++ src/components/ToyForm/ToyForm.jsx
@@ -59,9 +59,10 @@ const ToyForm = () => {
 
   return (
     <div>
-     
       <form id="input_form" onSubmit={handleSubmit} className="toy-form">
-      <div className="form-head"><p >Add a new toy!</p></div>
+        <div className="form-head">
+          <p>Add a new toy!</p>
+        </div>
         <label for="name">Name:</label>
         <input
           type="text"
@@ -101,100 +102,189 @@ const ToyForm = () => {
           }
         />
 
-        <fieldset id="age" value={newToy.age} className="toy-form-fieldset">
+        <fieldset id="age" value={newToy.age} className="toy-form-fieldset age">
           <legend>Age:</legend>
-          <label for="age1">
-            <input type="checkbox" id="age" name="age" value="1" />
-            0-2 year olds
-          </label>
-          <label for="age2">
-            <input type="checkbox" id="age" name="age" value="2" /> 2-4 year
-            olds
-          </label>
-          <label for="age3">
-            <input type="checkbox" id="age" name="age" value="3" /> 4-6 year
-            olds
-          </label>
-          <label for="age4">
-            <input type="checkbox" id="age" name="age" value="4" /> 7 and up
-          </label>
-          <label for="age5">
-            <input type="checkbox" id="age" name="age" value="5" /> 10 an up
-          </label>
-          <label for="age6">
-            <input type="checkbox" id="age" name="age" value="6" /> 12 and up
-          </label>
-          <label for="age7">
-            <input type="checkbox" id="age" name="age" value="7" /> Any age!
-          </label>
+          <section className="fieldset-row">
+            <label for="age1">
+              <input type="checkbox" id="age" name="age" value="1" />
+              0-2 year olds
+            </label>
+            <label for="age2">
+              <input type="checkbox" id="age" name="age" value="2" /> 2-4 year
+              olds
+            </label>
+            <label for="age3">
+              <input type="checkbox" id="age" name="age" value="3" /> 4-6 year
+              olds
+            </label>
+
+            <label for="age4">
+              <input type="checkbox" id="age" name="age" value="4" /> 7 and up
+            </label>
+          </section>
+          <section className="fieldset-row">
+            <label for="age5">
+              <input type="checkbox" id="age" name="age" value="5" /> 10 and up
+            </label>
+            <label for="age6">
+              <input type="checkbox" id="age" name="age" value="6" /> 12 and up
+            </label>
+            <label for="age7">
+              <input type="checkbox" id="age" name="age" value="7" /> Any age!
+            </label>
+          </section>
         </fieldset>
 
-        <fieldset id="categories" className="toy-form-fieldset">
+        <fieldset id="categories" className="toy-form-fieldset category">
           <legend>Categories:</legend>
-          <label for="Outdoors">
-            <input type="checkbox" id="category" name="categories" value="1" />
-            Outdoors
-          </label>
-          <label for="Sports">
-            <input type="checkbox" id="category" name="categories" value="2" />
-            Sports
-          </label>
-          <label for="STEM">
-            <input type="checkbox" id="category" name="categories" value="3" />
-            STEM
-          </label>
-          <label for="Art and Music">
-            <input type="checkbox" id="category" name="categories" value="4" />
-            Art and Music
-          </label>
-          <label for="Language/Reading">
-            <input type="checkbox" id="category" name="categories" value="5" />
-            Language/Reading
-          </label>
-          <label for="Play Pretend">
-            <input type="checkbox" id="category" name="categories" value="6" />
-            Play Pretend
-          </label>
-          <label for="Dolls/Figurines">
-            <input type="checkbox" id="category" name="categories" value="7" />
-            Dolls/Figurines
-          </label>
-          <label for="Animals">
-            <input type="checkbox" id="category" name="categories" value="8" />
-            Animals
-          </label>
-          <label for="Vehicles">
-            <input type="checkbox" id="category" name="categories" value="9" />
-            Vehicles
-          </label>
-          <label for="Tools">
-            <input type="checkbox" id="category" name="categories" value="10" />
-            Tools
-          </label>
-          <label for="Puzzles">
-            <input type="checkbox" id="category" name="categories" value="11" />
-            Puzzles
-          </label>
-          <label for="Games">
-            <input type="checkbox" id="category" name="categories" value="12" />
-            Games
-          </label>
-          <label for="Electronics">
-            <input type="checkbox" id="category" name="categories" value="13" />
-            Electronics
-          </label>
-          <label for="Building">
-            <input type="checkbox" id="category" name="categories" value="14" />
-            Building
-          </label>
-          <label for="Collectibles">
-            <input type="checkbox" id="category" name="categories" value="15" />
-            Collectibles
-          </label>
-          <label for="Sensory">
-            <input type="checkbox" id="category" name="categories" value="16" />
-            Sensory
-          </label>
+          <section className="fieldset-row">
+            <label for="Outdoors">
+              <input
+                type="checkbox"
+                id="category"
+                name="categories"
+                value="1"
+              />
+              Outdoors
+            </label>
+            <label for="Sports">
+              <input
+                type="checkbox"
+                id="category"
+                name="categories"
+                value="2"
+              />
+              Sports
+            </label>
+            <label for="STEM">
+              <input
+                type="checkbox"
+                id="category"
+                name="categories"
+                value="3"
+              />
+              STEM
+            </label>
+            <label for="Art and Music">
+              <input
+                type="checkbox"
+                id="category"
+                name="categories"
+                value="4"
+              />
+              Art and Music
+            </label>
+            <label for="Language/Reading">
+              <input
+                type="checkbox"
+                id="category"
+                name="categories"
+                value="5"
+              />
+              Language/Reading
+            </label>
+            <label for="Play Pretend">
+              <input
+                type="checkbox"
+                id="category"
+                name="categories"
+                value="6"
+              />
+              Play Pretend
+            </label>
+            <label for="Dolls/Figurines">
+              <input
+                type="checkbox"
+                id="category"
+                name="categories"
+                value="7"
+              />
+              Dolls/Figurines
+            </label>
+            <label for="Animals">
+              <input
+                type="checkbox"
+                id="category"
+                name="categories"
+                value="8"
+              />
+              Animals
+            </label>
+          </section>
+          <section className="fieldset-row">
+            <label for="Vehicles">
+              <input
+                type="checkbox"
+                id="category"
+                name="categories"
+                value="9"
+              />
+              Vehicles
+            </label>
+            <label for="Tools">
+              <input
+                type="checkbox"
+                id="category"
+                name="categories"
+                value="10"
+              />
+              Tools
+            </label>
+            <label for="Puzzles">
+              <input
+                type="checkbox"
+                id="category"
+                name="categories"
+                value="11"
+              />
+              Puzzles
+            </label>
+            <label for="Games">
+              <input
+                type="checkbox"
+                id="category"
+                name="categories"
+                value="12"
+              />
+              Games
+            </label>
+            <label for="Electronics">
+              <input
+                type="checkbox"
+                id="category"
+                name="categories"
+                value="13"
+              />
+              Electronics
+            </label>
+            <label for="Building">
+              <input
+                type="checkbox"
+                id="category"
+                name="categories"
+                value="14"
+              />
+              Building
+            </label>
+            <label for="Collectibles">
+              <input
+                type="checkbox"
+                id="category"
+                name="categories"
+                value="15"
+              />
+              Collectibles
+            </label>
+            <label for="Sensory">
+              <input
+                type="checkbox"
+                id="category"
+                name="categories"
+                value="16"
+              />
+              Sensory
+            </label>
+          </section>
         </fieldset>
 
         <button type="submit" className="button">
